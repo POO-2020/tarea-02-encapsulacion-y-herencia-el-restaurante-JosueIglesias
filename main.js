64e28734a7fab803b7cbf863aba9f6dc79f49f16
Josue Iglesias <jiglesias3@ucol.mx>
2020-03-17 14:41:46 -0600

    cliente frecuente

--- main.js
+++ main.js
@@ -7,9 +7,17 @@ import Tiempo from "./tiempo.js"
 import Fecha from "./fecha.js"
 import Pedido from "./pedido.js"
 import Restaurante from "./restaurante.js"
+import ClienteFrecuente from "./cliente-frecuente.js"
 
 const direccion1 = new Direccion("Av. Gordolova", 442, 2, "Las lomas", 32448, "Villa de Alvarez", "Villa de Alvarez")
 const cliente1 = new Cliente ("Enrique Pantoja", direccion1, "772-223-449")
+const datosClienteFrec1 = {
+    nombre: "Alfonsin Konika",
+    direccion: new Direccion ("Av. Gordolova", 441, 2, "Las lomas", 32448, "Villa de Alvarez", "Villa de Alvarez"),
+    telefono: "310-454-5323" ,
+    numeroCliente: 4,
+    fechaRegistro: new Fecha (2019, 3, 1)  
+}
 const datosPedido1 = {
     fecha: new Fecha(2020, 4, 24),
     hora: new Tiempo(1,50,"pm"),
@@ -34,12 +42,9 @@ class Main {
         this.producto1 = new Producto("Tortaogada estilo colimote", new Precio(49))
         this.producto2 = new Producto("Tacos de soia", new Precio(27))
         this.producto3 = new Producto("Agua de horchata", new Precio(15))
-        //this.direccion1 = new Direccion("Av. Gordolova", 442, 2, "Las lomas", 32448, "Villa de Alvarez", "Villa de Alvarez")
         this.elementoPedido1 = new ElementoPedido(this.producto1, 4)
         this.elementoPedido2 = new ElementoPedido(this.producto2, 5)
         this.elementoPedido3 = new ElementoPedido(this.producto3, 4)
-        //const cliente1 = new Cliente("Enrique Pantoja", this.direccion1, "772-223-449")
-        //this.restaurante = new Restaurante("El wen sazón", "333-688-345" ,new Direccion("Av. Lasoia", 343, 2, "Lomas doradas", 425523, "Villa de Alvarez", "Villa de Alvarez"))
         this.restaurante = new Restaurante ({
             nombre:"El wen sazón",
             telefono: "333-688-345",
@@ -48,38 +53,8 @@ class Main {
         this.pedido1 = new Pedido(datosPedido1)
         this.pedido2 = new Pedido(datosPedido2) 
         this.pedido3 = new Pedido(datosPedido3)
+        this.clienteFrec1 = new ClienteFrecuente (datosClienteFrec1)
         
-        /*
-        const datosPedido1 = {
-            fecha: new Fecha(2020, 4, 24),
-            hora: new Tiempo(1,50,"pm"),
-            cliente: cliente1
-        } 
-        const datosPedido2 = {
-            fecha: new Fecha(2020, 4, 25),
-            hora: new Tiempo (5, 30, "pm"),
-            cliente: cliente1
-        }
-        const datosPedido3 = {
-            fecha: new Fecha(2020, 3, 20),
-            hora: new Tiempo(4, 14, "pm"),
-            cliente: cliente1
-        }
-        /*this.datosPedido1 = {
-            fecha: new Fecha(2020, 4, 24),
-            hora: new Tiempo(1,50,"pm"),
-            cliente: this.cliente1
-        } 
-        this.datosPedido2 = {
-            fecha: new Fecha(2020, 4, 25),
-            hora: new Tiempo (5, 30, "pm"),
-            cliente: this.cliente1
-        }
-        this.datosPedido3 = {
-            fecha: new Fecha(2020, 3, 20),
-            hora: new Tiempo(4, 14, "pm"),
-            cliente: this.cliente1
-        }*/
     }
 
     probarPrecio(){
@@ -172,6 +147,11 @@ class Main {
         this.restaurante.listarPedidos()
 
     }
+
+    probarClienteFrecuente(){
+        console.log("---------Cliente Frecuente----------")
+        console.log(this.clienteFrec1.getPerfil())
+    }
 }
 
 let app = new Main
@@ -184,3 +164,4 @@ app.probarTiempo()
 app.probarFecha()
 app.probarPedido()
 app.probarRestaurante()
+app.probarClienteFrecuente()
